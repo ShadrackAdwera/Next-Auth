c9e586e797d74ceb8c4da7753b4b8b18d03f58b5
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-20 05:55:50 +0300

    add API authentication route using next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import { dbConnect } from '../../../utils/db';
+import { comparePasswords } from '../../../utils/auth';
+
+
+export default NextAuth({
+    session: {
+        jwt: true
+    },
+    providers: [
+        Providers.Credentials({
+            async authorize(credentials) {
+                const client = await dbConnect();
+                const usersCollection = client.db();
+                const foundUser = await usersCollection.collection('users').findOne({email: credentials.email});
+                if(!foundUser) {
+                client.close();
+                throw new Error('User not found');
+                }
+
+                const passwordCheck = await comparePasswords(credentials.password, foundUser.password);
+                if(!passwordCheck) {
+                client.close();
+                throw new Error('Invalid Password');
+                }
+                client.close();
+                return {
+                    email: foundUser.email
+                }
+            }
+        })
+    ]
+})
