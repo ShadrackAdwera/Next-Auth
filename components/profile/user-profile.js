dce1129b8bf017e1215a3eaaabbd2672c49fdda1
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-20 06:55:52 +0300

    add server side route guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
-import { getSession } from 'next-auth/client';
+// import { useEffect, useState } from 'react';
+// import { getSession } from 'next-auth/client';
 
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
   // Redirect away if NOT auth
-  const [isLoading, setIsLoading] = useState(true);
+  //const [isLoading, setIsLoading] = useState(true);
   // const [loadedSession, setLoadedSession] = useState();
 
-  useEffect(()=>{
-    getSession().then(session=>{
-      if(!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-      //setLoadedSession(session);
-      //purpose of this hook is just for navigating away if we have no session
-    })
-  },[])
+  // useEffect(()=>{
+  //   getSession().then(session=>{
+  //     if(!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //     //setLoadedSession(session);
+  //     //purpose of this hook is just for navigating away if we have no session
+  //   })
+  // },[])
 
-  if(isLoading) {
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if(isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
 
   return (
     <section className={classes.profile}>
