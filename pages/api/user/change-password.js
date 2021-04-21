36c14459c6709e962dcac1f4735c70e52ae5b2f4
shadrack.adwera <adweshshaddie@gmail.com>
2021-04-21 06:28:30 +0300

    add the change password api route

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from 'next-auth/client';
+import { comparePasswords, hashPassword } from '../../../utils/auth';
+import { dbConnect } from '../../../utils/db';
 
 export default handler = async(req,res) => {
     if(req.method==='PATCH') {
@@ -6,8 +8,27 @@ export default handler = async(req,res) => {
         if(!session) {
             res.status(401).json({message: 'Auth failed!'});
             return;
+        } 
+        const email = session.user.email;
+        const { oldPassword, newPassword } = req.body;
+        const client = await dbConnect();
+        const db = client.db();
+        const foundUser = db.collection('Users').findOne({email: email});
+        if(!foundUser) {
+            res.status(404).json({message: 'User not found'})
+            client.close();
+            return;
         }
-        
+        const isPassword = await comparePasswords(foundUser.password, oldPassword);
+        if(!isPassword) {
+            res.status(422).json({message: 'Passwords do not match'});
+            client.close();
+            return;
+        }
+        const hashedPassword = await hashPassword(newPassword);
+        await db.collection('Users').updateOne({email: email},{$set: {password: hashedPassword}});
+        res.status(200).json({message: 'Password updated'});
+        client.close();
     }
 
 }
--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,6 @@ import { MongoClient } from 'mongodb';
 export const dbConnect = async() => {
     try {
         const client = await MongoClient.connect(process.env.DB_URL);
-        console.log('COnnecting to DB...');
         return client;
     } catch (error) {
         
